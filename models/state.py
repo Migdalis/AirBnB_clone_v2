3005ad4ca039a316abcac3c0398c8b8ebfcf30c9
Migdalis <miglepage@gmail.com>
2022-05-17 13:16:01 -0500

    :hammer: Try some to task 5. Modified cascade in state file

--- models/state.py
+++ models/state.py
@@ -2,9 +2,9 @@
 """ State Module for HBNB project """
 import models
 from os import getenv
-from hashlib import new
 from models.base_model import BaseModel, Base
 from models.city import City
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -13,14 +13,13 @@ class State(BaseModel, Base):
     """ Class to handle a State """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all")
+    cities = relationship("City", backref="state")
 
     def __init__(self, *args, **kwargs):
         """Constructor to a new instance of state"""
         super().__init__(self, *args, **kwargs)
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
-        name = ""
         @property
         def citties(self):
             """Returns the list of City instances with state_id equals to the current State.id"""
