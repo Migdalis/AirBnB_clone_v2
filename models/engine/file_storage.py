c29aef17dec1fe832342a2b1e5fd78ef096aec90
Migdalis <miglepage@gmail.com>
2022-05-17 12:09:55 -0500

    :hammer: Try some in file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,11 +18,11 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is None:
-            return FileStorage.__objects
+            return self.__objects
         else:
             new_dict = {}
             for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
+                if cls == value.__class__:
                     new_dict[key] = value
             return new_dict
 
