704baeb9a58b294b13516dc096c37dffc7366a77
Migdalis <miglepage@gmail.com>
2022-05-17 16:47:25 -0500

    :hammer: Try fix issues with thw file storage

--- models/base_model.py
+++ models/base_model.py
@@ -54,8 +54,8 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
 
-        if '_sa_instance_state' in self.__dict__:
-            del self.__dict__['_sa_instance_state']
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,13 +2,6 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 import models
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 class FileStorage:
@@ -42,11 +35,19 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
                     'Review': Review
-                  }
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
