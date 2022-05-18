ac305a80dfd8b744f830a84ba1c1451f7c4f67dc
Migdalis <miglepage@gmail.com>
2022-05-17 19:54:52 -0500

    :hammer: Change Amenity model

--- models/amenity.py
+++ models/amenity.py
@@ -10,7 +10,7 @@ class Amenity(BaseModel, Base):
     """Class to handle a amenity"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity", backref="amenities")
+    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
 
     def __init__(self, *args, **kwargs):
         """Constructor to a new instance of amenity"""
