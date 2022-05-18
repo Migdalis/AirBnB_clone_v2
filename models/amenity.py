78405881c6c2f5a04231bdaa5a49d9c8cad75823
Migdalis <miglepage@gmail.com>
2022-05-17 19:49:29 -0500

    :hammer: Add Place-Amenity relation many to many

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Class to handle a amenity"""
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity", backref="amenities")
 
     def __init__(self, *args, **kwargs):
         """Constructor to a new instance of amenity"""
--- models/place.py
+++ models/place.py
@@ -3,10 +3,20 @@
 from os import getenv
 from models.base_model import BaseModel, Base
 from models.review import Review
+from models.amenity import Amenity
 import models
 import sqlalchemy
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60), ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60), ForeignKey("amenities.id",
+                             primary_key=True, nullable=False))
+                )
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -22,6 +32,7 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place", cascade="delete")
+    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
     amenity_ids = []
 
     def __init__(self, *args, **kwargs):
@@ -38,3 +49,19 @@ class Place(BaseModel, Base):
                 if rvw.place_id == self.id:
                     new_list.append(rvw)
             return new_list
+
+        @property
+        def amenities(self):
+            """Returns the list of Amenity instances linked to the Place"""
+            new_list =[]
+            all_amenities = models.storage.all(Amenity)
+            for amnt in all_amenities.values():
+                if amnt.id in self.amenity_ids:
+                    new_list.append(amnt)
+            return new_list
+
+        @amenities.setter
+        def amenities(self, amenity=None):
+            """Setter method for add an Amenity.id"""
+            if type(amenity) == Amenity:
+                self.amenity_ids.append(amenity.id)
