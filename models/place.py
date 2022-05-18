f7568d74802f4a9e009a5a7a9b3cf5e355dfbb00
Migdalis <miglepage@gmail.com>
2022-05-17 20:46:07 -0500

    :hammer: Try fix

--- models/place.py
+++ models/place.py
@@ -10,7 +10,7 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 
-place_amenity = Table("place_amenity", Base.metadata,
+table_place_amenity = Table("place_amenity", Base.metadata,
                       Column("place_id", String(60), ForeignKey("places.id"),
                              primary_key=True, nullable=False),
                       Column("amenity_id", String(60), ForeignKey("amenities.id"),
