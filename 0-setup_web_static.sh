1d44d626874a994b9a2fe6ed8a89e382da9b18ab
Migdalis <miglepage@gmail.com>
2022-05-24 21:19:48 -0500

    :white_check_mark: Change setup file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo echo "<html> <body> Holberton School </body> </html>" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/server_name _;/a\\n\tlocation \/hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
