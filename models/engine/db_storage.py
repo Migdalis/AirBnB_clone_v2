33b2815f1d21a3d993bc15e7e67b872164e008df
Migdalis <miglepage@gmail.com>
2022-06-10 18:03:56 -0500

    :white_check_mark: Add task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,7 @@ class DBStorage:
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session()
+
+    def close(self):
+        """ Close session """
+        self.__session.close()
